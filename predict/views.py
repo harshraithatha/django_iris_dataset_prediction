a4d4fa92bd6384b5f04c1cf5beea291efd07fa3e
Harsh Raithatha <40994481+harshraithatha@users.noreply.github.com>
2022-08-17 15:45:57 +0530

    Add files via upload

---
+++ predict/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import pandas as pd
+from .models import PredResults
+from pathlib import Path
+
+INPUT_DIR = Path.home() / 'OneDrive - kyndryl' / 'Files' / 'Sensitive' / 'Education Documents' / 'Great Lakes' / 'Anaconda' / 'Django' / 'Iris'
+OUTPUT_DIR = Path.home() / 'OneDrive - kyndryl' / 'Files' / 'Sensitive' / 'Education Documents' / 'Great Lakes' / 'Anaconda' / 'Django' / 'Iris'
+MODEL_DIR = INPUT_DIR / 'model'
+
+
+def predict(request):
+    return render(request, 'predict.html')
+
+
+def predict_chances(request):
+
+    if request.POST.get('action') == 'post':
+
+        # Receive data from client
+        sepal_length = float(request.POST.get('sepal_length'))
+        sepal_width = float(request.POST.get('sepal_width'))
+        petal_length = float(request.POST.get('petal_length'))
+        petal_width = float(request.POST.get('petal_width'))
+
+        # Unpickle model
+        model = pd.read_pickle(MODEL_DIR / "iris_model.pickle")
+        # Make prediction
+        result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+
+        classification = result[0]
+
+        PredResults.objects.create(sepal_length=sepal_length, sepal_width=sepal_width, petal_length=petal_length,
+                                   petal_width=petal_width, classification=classification)
+
+        return JsonResponse({'result': classification, 'sepal_length': sepal_length,
+                             'sepal_width': sepal_width, 'petal_length': petal_length, 'petal_width': petal_width},
+                            safe=False)
+
+
+def view_results(request):
+    # Submit prediction and show all
+    data = {"dataset": PredResults.objects.all()}
+    return render(request, "results.html", data)
